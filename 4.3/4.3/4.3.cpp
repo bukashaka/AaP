7426fda8f5f6e922d43077c11ac49a9075bbcab5
bukashaka <snngame140404@mail.ru>
2022-09-18 23:34:13 +0500

    1 homework

---
+++ 4.3/4.3/4.3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	long double a, b, c;
+	std::cout << "Vvedite dlini storon treugolnika";
+	std::cin >> a >> b;
+	std::cout << "Vvedite ugol mezdu storonami";
+	std::cin >> c;
+	std::cout << static_cast<double>(1) / 2 * a * b * sin(c);
+};
---
+++ 5.6/5.6/5.6.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    double  a, b, c, d, e;
+    cout << "Vvedite summu vklada(rub.):";
+    cin >> c;
+    cout << "Vvedite srok vklada(dni):";
+    cin >> b;
+    cout << "Vvedite procentnuyu stavku(% godovie):";
+    cin >> a;
+    d = ((a / 100 * c) / 365) * b;
+    e = d + c;
+    cout << "Dohod: " << d << " rub." << endl;
+    cout << "Summa: " << e << " rub.";
+}
